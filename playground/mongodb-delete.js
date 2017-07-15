2366e5763fb9b5e41dc69783b32a9338aba11f66
John Huang <johnhuang1225@gmail.com>
2017-07-15 08:09:22 +0800

    Add delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -7,11 +7,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB Server');
 
-    db.collection('Users').find().toArray().then((docs) => {
-        console.log('Users');
-        console.log(JSON.stringify(docs, undefined, 2));
-    }, (err) => {
-        console.log('Unabel to fetch todos');
+    db.collection('Users').findOneAndDelete({name: 'johnhuang'}).then((result) => {
+        console.log(result);
     });
 
 
