b8be93b79c4315fe5d652f7dbc636ce21acdb554
John Huang <johnhuang1225@gmail.com>
2017-07-17 10:04:51 +0800

    Add POST /todos route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+
+module.exports = {
+    Todo
+}
--- server/server.js
+++ server/server.js
@@ -1,29 +1,54 @@
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+var app = express();
+app.use(bodyParser.json());
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+
 });
 
-var newTodo = new Todo({
-    text: '餵貓',
-    completed: true,
-    completedAt: 1234
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
 
-newTodo.save().then((doc) => {
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-    console.log('Unable to save todo');
-});
+
+
+// var newTodo = new Todo({
+//     text: '餵貓',
+//     completed: true,
+//     completedAt: 1234
+// });
+//
+// newTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+
+// var jessica = new User({
+//     email: 'abcjessicabc@gmail.com'
+// });
+//
+// jessica.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log(e);
+// });
